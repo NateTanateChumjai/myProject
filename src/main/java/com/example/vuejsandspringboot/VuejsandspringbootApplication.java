2932e885aeff3db08d0f6fe84a60a6f79438c834
tanate <tanate.chu@gmail.com>
2020-09-02 17:01:03 +0700

    add new file

---
+++ src/main/java/com/example/vuejsandspringboot/VuejsandspringbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.vuejsandspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VuejsandspringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VuejsandspringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/vuejsandspringboot/VuejsandspringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vuejsandspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VuejsandspringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
